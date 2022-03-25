0ea768df412e5f2a80c9ece19e63b20596c235a9
Preent Loo <psilocybit5211@gmail.com>
2022-03-25 10:33:12 -0300

    Primer commit del proyecto.

---
+++ app.js
@@ -0,0 +1,12 @@
+/*Node por defecto trabaja de forma síncriona/secuencial
+NO de forma asíncrona*/
+require("colors");
+console.clear();
+
+
+const main = async() => {
+    console.log("Kunamasta".rainbow);
+};
+
+
+main();
