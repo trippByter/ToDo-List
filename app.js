eb5871a12c55bf7efa927ad019c4732efc761021
Preent Loo <psilocybit5211@gmail.com>
2022-03-25 12:38:29 -0300

    stdin-stdout-readline. Mostramos menú y esperamos input de user.

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 /*Node por defecto trabaja de forma síncriona/secuencial
 NO de forma asíncrona*/
+// Los imports se ordenan: 
+// primero los de terceros
+// luego las nuestras
 require("colors");
+const { mostrarMenu } = require("./helpers/mensajes");
+
 console.clear();
 
 
 const main = async() => {
     console.log("Kunamasta".rainbow);
+    mostrarMenu();
 };
 
 
---
+++ helpers/mensajes.js
@@ -0,0 +1,37 @@
+require("colors");
+
+const mostrarMenu = () => {
+    // Mostramos el menú en pantalla
+    console.clear();
+    console.log("  ==========================".yellow);
+    console.log("    SELECCIONE UNA OPCIÓN ".cyan);
+    console.log("  ==========================\n".yellow);
+    console.log(`  ${"1.".green} Crear tarea`);
+    console.log(`  ${"2.".green} Listar tareas`);
+    console.log(`  ${"3.".green} Listar tareas completadas`);
+    console.log(`  ${"4.".green} Listar tareas pedientes`);
+    console.log(`  ${"5.".green} Completar tarea(s)`);
+    console.log(`  ${"6.".green} Borrar tarea`)
+    console.log(`  ${"0.".green} Salir\n`);
+    
+    // 'readline' ya viene en node
+    // Creamos interface para recibir
+    // y enviar info por consola
+    const readline = require("readline").createInterface({
+        input: process. stdin,
+        output: process.stdout
+    });
+
+    // Se usa con stdout para mostrar pregunta
+    // En el callback se recibe la info
+    readline.question("Selecione una opción: ".bold.blue, (opt) => {
+        // Mostramos en input de la consola
+        console.log(opt);
+        // Para no esperar más info del usuario
+        readline.close();
+    });
+}
+
+module.exports = {
+    mostrarMenu
+}
